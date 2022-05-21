d9b15d5d2ca4eef0d6c689ec56510e8150c9dc4a
willy <drexie16@gmail.com>
2022-05-21 20:23:15 +0700

    add sample config

---
+++ sample-config/config.go
@@ -0,0 +1,34 @@
+package config_sample
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+var (
+	DefaultConfig = "./config.yaml"
+	Debug         bool
+)
+
+func ReadConfig() {
+	debug := flag.Bool("debug", Debug, "debug mode")
+	flag.Parse()
+	Debug = *debug
+
+	files, _ := ioutil.ReadDir("config/")
+
+	viper.AddConfigPath("config/")
+	for _, file := range files {
+		name := strings.Split(file.Name(), ".")[0]
+		viper.SetConfigName(name)
+		//handle all config
+		err := viper.MergeInConfig()
+		if err != nil {
+			log.Fatalf("[Server] reading config file %s \n", err)
+		}
+	}
+}
